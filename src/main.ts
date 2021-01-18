e0419d4c621821b3d53e000c7503b8dd660e115e
Josh Wulf <josh@magikcraft.io>
2021-01-19 10:48:11 +1300

    Use env
    Signed-off-by: Josh Wulf <josh@magikcraft.io>

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,15 @@ import { config } from "dotenv";
 
 config();
 
-const clientId = process.env.clientId;
-const clientSecret = process.env.clientSecret;
-const clusterId = process.env.clusterId;
+const clientId = process.env.clientId!;
+const clientSecret = process.env.clientSecret!;
+const clusterId = process.env.clusterId!;
 
 const zbc = new ZBClient({
   camundaCloud: {
-    clientId: "9l.8gLFqM3wHaA-qQITjNMG0fkipg~-Z",
-    clientSecret:
-      "vGNZCTP5KzUJWCs6_fFQ2lv5-3GQj~cTcwXu7Abk7TxG.As6IYg2UDoRAvtr7e5Z",
-    clusterId: "3b640f45-0dcd-469c-8551-7f68a5d4f54b",
+    clientId,
+    clientSecret,
+    clusterId,
   },
 });
 
